64b353c90c51fadaa5ea79be23eb8f16f055a848
siphesande <siphesande@projectcodex.co>
2015-07-03 10:03:53 +0200

    add as project dependency

--- hello_test.js
+++
@@ -1,5 +0,0 @@
-TestMyCode.run("testing hello function", function(assert) {
-    var result = hello();
-    // is the result as we expected?
-    assert.equals("hello", result, "testing hello function");
-});
