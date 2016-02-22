76f3fba85d084b2d600f7410aa24bea079302e5e
siphesande <siphesande@projectcodex.co>
2016-02-22 10:33:53 +0200

    Update uppercase_letters.js

--- uppercase_letters.js
+++ uppercase_letters.js
@@ -1,22 +1,3 @@
-/*function uppercase_letters(aCharacter)
-        {
-            return (aCharacter >= 'A') && (aCharacter <= 'Z');
-        }*/
-
-/* function uppercase_letters(inputtxt)  
-  {  
-   var letters = /^[A-Za-z]+$/;  
-   if(inputtxt.value.match(letters))  
-     {  
-      return true;  
-     }  
-   else  
-     {  
-     alert("message");  
-     return false;  
-     }  
-  }  
-*/
 
 function uppercase_letters(str)
 {
