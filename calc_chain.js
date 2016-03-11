24a953c651f128653f71a2282f7c6ba45095fdf5
siphesande <siphesande@projectcodex.co>
2016-03-11 08:07:29 +0200

    spaza-app

--- calc_chain.js
+++ calc_chain.js
@@ -1,70 +1,88 @@
-// //function calc_chain(){
-// var Calc = function(){
-//   this.sum = 0; //could be a param
-//   return this;
-// };
-
-// Calc.prototype.add = function(x){
-//   this.sum = this.sum + x;
-//   return this;
-// };
-
-// Calc.prototype.multiply = function(x){
-//   this.sum = this.sum * x;
-//   return this;
-// };
-  
-// Calc.prototype.subtract = function(x){
-//   this.sum = this.sum - x;
-//   return this;
-// };
-
-// Calc.prototype.calce_chain = function(){
-//   return this.sum;
-// };
-  
-// console.log(new Calc().add(1).add(2).multiply(3).subtract(4).calce_chain());
-// //return new Calc().add(1).add(2).multiply(3).subtract(4).calce_chain();
 function calc_chain(){
+var Calc = function(){
+  this.sum = 0; //could be a param
+  return this;
+};
 
-var value = function(val){
-  return function(){
-    return val;
-  }
-}
+Calc.prototype.add = function(x){
+  this.sum = this.sum + x;
+  return this;
+};
 
-// A function to add two functions together
-var add = function(x, y){
-  return function(){
-   return x() + y();
-  }
-}
+Calc.prototype.multiply = function(x){
+  this.sum = this.sum * x;
+  return this;
+};
+  
+Calc.prototype.subtract = function(x){
+  this.sum = this.sum - x;
+  return this;
+};
+Calc.prototype.divide = function(x){
+  this.sum = this.sum % x;
+  return this;
+};
 
-// A function to subtract one function from another
-var subtract = function(x, y){
-  return function(){
-    return x() - y();
-  }
-}
+Calc.prototype.calce_chain = function(){
+  return this.sum;
+};
+  
+console.log(new Calc().add(1).add(2).multiply(3).subtract(4).calce_chain());
+return new Calc().add(1).add(2).multiply(3).subtract(4).divide(7).calce_chain();
+};
 
-// A function to multiply two functions
-var multiply = function(x, y){
-  return function(){
-    return x() * y();
-  }
-}
 
-var one = value(1);
-var two = value(2);
-var three = value(3);
-var four = value(4);
 
-// (1 + 2) * 3 - 4 == 5
-var resultFunc = subtract(multiply(add(one, two), three), four);
+
+// <<<----------                OR                  --------------->>>
+
+
+
+// var calc_chain = function(){
+
+// var value = function(val){
+//   return function(){
+//     return val;
+//   }
+// }
+
+// // A function to add two functions together
+// var add = function(x, y){
+//   return function(){
+//    return x() + y();
+//   }
+// }
+
+// // A function to subtract one function from another
+// var subtract = function(x, y){
+//   return function(){
+//     return x() - y();
+//   }
+// }
+
+// // A function to multiply two functions
+// var multiply = function(x, y){
+//   return function(){
+//     return x() * y();
+//   }
+// }
+
+// var one = value(1);
+// var two = value(2);
+// var three = value(3);
+// var four = value(4);
+
+// // (1 + 2) * 3 - 4 == 5
+
+
+// 	console.log(subtract(multiply(add(one, two), three), four));
+// 	return subtract(multiply(add(one, two), three), four);
+// }
 
 // but it's still a function right now, so evaluate it
-var result = resultFunc();
-console.log(result); // => 5
-return results;
-}
+//var result = calc_chain();
+//console.log(result); // => 5
+// return results;
+
+
 
